4cf023ee3c3a0b8faf445982c1695c6b911b8df6
Claudio Costa <costa.pederneschi@gmail.com>
2024-02-10 20:53:29 -0300

    vercel

---
+++ components/text.js
@@ -0,0 +1,18 @@
+import { useState } from "react"
+import { Styles } from "../pages/styles";
+
+function Text(props){
+    const [numero, setContador] = useState(1);
+
+    function contador(){
+        setContador(numero + 10)
+    }
+
+    return  <div>
+                <div>{numero}</div>
+                <button onClick={contador}>Adicionar</button>
+                <Styles fontsize={numero}>Hello</Styles>
+            </div>
+}
+
+export default Text
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,10 @@
-import { useState } from "react"
+import Text from "../components/text"
 import {Styles} from "./styles"
 
 function Home() {
     return <Text>Home</Text>
 }
 
-function Text(props){
-    const [numero, setContador] = useState(1);
-
-    function contador(){
-        setContador(numero + 10)
-    }
-
-    return  <div>
-                <div>{numero}</div>
-                <button onClick={contador}>Adicionar</button>
-                <Styles fontsize={numero}>Hello</Styles>
-            </div>
-}
 
 
 export default Home
