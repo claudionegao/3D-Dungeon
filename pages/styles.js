60acfa6f3512d509bd70995a094c7e1ef6a64bf1
Claudio Costa <costa.pederneschi@gmail.com>
2024-02-10 20:55:32 -0300

    Update styles.js

--- pages/styles.js
+++ pages/styles.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
-export const Styles = styled.h1`
+const Styles = styled.h1`
     color: red;
     font-size: ${props => `${props.fontsize}px`};
 `
 
-export const Styles2 = styled(Styles)`
+const Styles2 = styled(Styles)`
     
 `
+
+export default Styles
